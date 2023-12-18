6c1478ab9db3140c6009341628096ebce137b101
Abdallah Mahmoud Ahmed Mahmoud Attallah <abdallahattallah197@gmail.com>
2023-12-18 08:46:21 +0200

    Modify the profile schema

--- src/models/profiles.js
+++ src/models/profiles.js
@@ -5,9 +5,31 @@ const Schema = mongoose.Schema;
 // Create a profile schema object
 const profileSchema = new Schema({
   name: { type: String, required: true },
-  phone: { type: Number, required: true },
-  age: { type: Number, required: true },
-  email: { type: String, required: true },
+  phone: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function (v) {
+        // Add phone number validation logic
+        return /\d{10}/.test(v); // Example: 10-digit number
+      },
+      message: "Phone number is not valid",
+    },
+  },
+  age: { type: Number, min: 18, required: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function (v) {
+        // Add email validation logic
+        return /\S+@\S+\.\S+/.test(v);
+      },
+      message: "Email is not valid",
+    },
+  },
 });
 
 // Create a model based on the schema
