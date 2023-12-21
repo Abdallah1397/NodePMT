896de092d42439ad18c3ed54afe8fd479ccf5431
Abdallah Mahmoud Ahmed Mahmoud Attallah <abdallahattallah197@gmail.com>
2023-12-21 17:56:16 +0200

    Add the routes entry point

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const {
-  getAllProfiles,
-  getSingleProfile,
-  deleteProfile,
-  createNewProfile,
-  updateExistingProfile,
-} = require("../controllers/profileController");
+const profileRouter = require("./profiles");
+const authRouter = require("./auth");
 
-// Routers
-router.get("/profiles", getAllProfiles);
-router.get("/profiles/:id", getSingleProfile);
-router.post("/profiles", createNewProfile);
-router.put("/profiles/:id", updateExistingProfile);
-router.delete("/profiles/:id", deleteProfile);
+router.use("/profiles", profileRouter);
+router.use("/auth",authRouter);
 
 module.exports = router;
